7874d23892b5930685ce69976fe899b13bb677ea
muhzinmuhammed <muhzinsidhiq333@gmail.com>
2024-08-28 19:19:24 +0530

    .

--- src/features/api/common.js
+++ src/features/api/common.js
@@ -1,7 +1,8 @@
 export const all={
     // baseurl:"http://192.168.1.225:8000/api"
-    baseurl:"http://localhost:8080/api",
-    // baseurl:"https://qurinom-backend-evjl.onrender.com/api",//mozhin
+    // baseurl:"http://localhost:8080/api",
+   //mozhin
+   baseurl:"https://icore-server.onrender.com/api"
     
     
 }
